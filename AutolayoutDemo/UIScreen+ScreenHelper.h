8c0f11d639d3318ea290b4328602a34cee52062a
liubiao <liubiao@didichuxing.com>
2016-07-25 18:41:56 +0800

    update

---
+++ AutolayoutDemo/UIScreen+ScreenHelper.h
@@ -0,0 +1,46 @@
+//
+//  UIScreen+ScreenHelper.h
+//  DiCarpool
+//
+//  Created by alex520biao on 15/6/12. Maintain by alex520biao
+//  Copyright (c) 2015年 DiDi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//根据屏幕尺寸自动选择长度
+#define SSLNG(s, m, n, l) [UIScreen lengthWithSmall:s medium:m normal:n large:l]
+
+@interface UIScreen (ScreenHelper)
+
+/*!
+ *  @brief  所有iPhone屏幕尺寸分类(四种类型)
+ *
+ */
+typedef NS_ENUM(NSInteger, UIScreenSizeType) {
+    UIScreenSizeType_Small    = 0,  // 320*480及以下 iPhone4s及其以前设备
+    UIScreenSizeType_Medium   = 1,  // 320*568      iPhone5/5C/5S
+    UIScreenSizeType_Normal   = 2,  // 375*667      iPhone6
+    UIScreenSizeType_Large    = 3   // 414*736及更大 iPhone6+及其更大
+};
+
+#pragma mark - screenSize
++ (UIScreenSizeType)screenSizeType;
+
+/*!
+ *  @brief  根据屏幕尺寸自动选择长度
+ *
+ *  @param smallLength  屏幕尺寸为UIScreenSizeType_Small时长度
+ *  @param mediumLength 屏幕尺寸为UIScreenSizeType_Medium时长度
+ *  @param normalLength 屏幕尺寸为UIScreenSizeType_Normal时长度
+ *  @param largeLength  屏幕尺寸为UIScreenSizeType_Large时长度
+ *
+ *  @return
+ */
++ (CGFloat)lengthWithSmall:(CGFloat)smallLength
+                    medium:(CGFloat)mediumLength
+                    normal:(CGFloat)normalLength
+                     large:(CGFloat)largeLength;
+
+
+@end
---
+++ AutolayoutDemo/UIScreen+ScreenHelper.m
@@ -0,0 +1,72 @@
+//
+//  UIScreen+ScreenHelper.m
+//  DiCarpool
+//
+//  Created by alex520biao on 15/6/12. Maintain by alex520biao
+//  Copyright (c) 2015年 DiDi. All rights reserved.
+//
+
+#import "UIScreen+ScreenHelper.h"
+
+@implementation UIScreen (ScreenHelper)
+
+#pragma mark - screenSize
++ (UIScreenSizeType)screenSizeType{
+    int screenWidth  = [UIScreen mainScreen].bounds.size.width;
+    int screenHeight = [UIScreen mainScreen].bounds.size.height;
+    
+    UIScreenSizeType screenSizeType = UIScreenSizeType_Normal;
+    // 320*480及以下 iPhone4s及其以前设备
+    if (screenWidth == 320 && screenHeight == 480) {
+        screenSizeType = UIScreenSizeType_Small;
+    }
+    // 320*568      iPhone5/5C/5S
+    else if (screenWidth == 320 && screenHeight == 568){
+        screenSizeType = UIScreenSizeType_Medium;
+    }
+    // 375*667      iPhone6
+    else if (screenWidth == 375 && screenHeight == 667 ){
+        screenSizeType = UIScreenSizeType_Normal;
+    }
+    // 414*736及更大 iPhone6+及其更大
+    else if (screenWidth >= 414 && screenHeight >= 736){
+        screenSizeType = UIScreenSizeType_Large;
+    }
+    
+    //    NSLog(@"screenWidth:%d,screenHeight:%d",screenWidth,screenHeight);
+    //    NSLog(@"screenSizeType:%ld",(long int)screenSizeType);
+    return screenSizeType;
+}
+
++ (CGFloat)lengthWithSmall:(CGFloat)smallLength
+                    medium:(CGFloat)mediumLength
+                    normal:(CGFloat)normalLength
+                     large:(CGFloat)largeLength{
+    UIScreenSizeType screenSizeType = [UIScreen screenSizeType];
+    CGFloat length = normalLength;
+    switch (screenSizeType) {
+        case UIScreenSizeType_Small:{
+            length = smallLength;
+            break;
+        }
+        case UIScreenSizeType_Medium:{
+            length = mediumLength;
+            break;
+        }
+        case UIScreenSizeType_Normal:{
+            length = normalLength;
+            break;
+        }
+        case UIScreenSizeType_Large:{
+            length = largeLength;
+            break;
+        }
+        default:{
+            length = normalLength;
+            break;
+        }
+    }
+    return length;
+}
+
+@end
